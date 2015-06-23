75b838747231b8a369f36d0e59c03e2ecb7b40d9
akeshwarj <akeshwar@zemosolabs.com>
2015-06-23 14:34:59 +0530

    Update ZAlgebra.java

--- ZAlgebra.java
+++ ZAlgebra.java
@@ -1,10 +1,17 @@
 
 public class ZAlgebra {
 	public static void main(String[] args){
-		Zdigit zd = new Zdigit('0');
-		ZNumber zn = new ZNumber('A','B','C','D');
-		zn.toDecimal();
-		ZNumber zn1 = new ZNumber("ABCDE");
+		//ZDigit zd = new ZDigit('0');
+		ZNumber zn1 = new ZNumber('A','B','C','D');
 		zn1.toDecimal();
+		
+		ZNumber zn2 = new ZNumber("ABCDE");
+		zn2.toDecimal();
+		
+		ZNumber zn3 = new ZNumber(new char[]{'P','Q','R','S'});
+		zn3.toDecimal();
+		
+		
+		//ZDigit zn[] = zn3.getDigits();
 	}
 }
