ae6c50490af9125cfc0bdd241a08c73156541090
akeshwarj <akeshwar@zemosolabs.com>
2015-06-23 14:36:04 +0530

    Update and rename Zdigit.java to ZDigit.java

--- ZDigit.java
+++ ZDigit.java
@@ -1,8 +1,11 @@
-public final class Zdigit {
-	public Zdigit(char input){
-		int ascii = (int) input;  
+
+public final class ZDigit {
+	public ZDigit(char input){
+		final int ascii = (int) input;  
 		if (ascii != 48 && (ascii < 65 || ascii > 90)) {
 			    throw (new IllegalArgumentException("Please enter a ZDigit only (0, A-Z)!"));
 		}
+		else
+			System.out.println(input);
 	}
 }
