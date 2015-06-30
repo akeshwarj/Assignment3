aa0cc18105758ee577fd8ab8c7c4171e3555fb60
akeshwarj <akeshwar@zemosolabs.com>
2015-06-30 11:50:55 +0530

    Update ZDigit.java

--- ZDigit.java
+++ ZDigit.java
@@ -3,11 +3,11 @@ public final class ZDigit {
 	public static String storeZDigit = "";
 	
 	public ZDigit(char input) {
-		final int ascii = (int) input;
-		if (ascii != 48 && (ascii < 65 || ascii > 90)) {
+		if (input != '0' && (input < 'a' || input > 'z')) {
 			    throw (new IllegalArgumentException("Please enter a ZDigit only (0, A-Z)!"));
 		} else {
 			storeZDigit = storeZDigit + input;
+			System.out.println();
 		}
 	}
 }
