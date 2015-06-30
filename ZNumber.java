61faab0935c309bac98bafcdf5dc5b47f32bc567
akeshwarj <akeshwar@zemosolabs.com>
2015-06-30 10:38:23 +0530

    Update ZNumber.java

--- ZNumber.java
+++ ZNumber.java
@@ -1,52 +1,53 @@
 
 public final class ZNumber {
-	private final String Znum;				//Znum is the ZAlgebra version before being converted to Decimal form
+	private final String zNum;				//zNum is the ZAlgebra version before being converted to Decimal form
 	int length;
 	
-	public ZNumber(char... Zdigits){		//CONSTRUCTOR NUMBER 1 for character array input
-		Znum = new String(Zdigits);
+	public ZNumber(char... zDigits) {		//CONSTRUCTOR NUMBER 1 for character array input
+		zNum = new String(zDigits);
 	}	
 	
-	public ZNumber(String Zdigits){			//CONSTRUCTOR NUMBER 2 for string input
-		Znum = Zdigits;
+	public ZNumber(String zDigits) {			//CONSTRUCTOR NUMBER 2 for string input
+		zNum = zDigits;
 	}
 	
 	
 	//Implement a method on ZNumber: public ZDigit[] getDigits(), it would return the zdigits
-	public final ZDigit[] getDigits(){
-		int l = Znum.length();
+	public final ZDigit[] getDigits() {
+		int l = zNum.length();
 		ZDigit[] array = new ZDigit[l];
 		return array;
 	}
 	
 	
-	@Override public String toString(){
-		return Znum;
+	@Override public String toString() {
+		return zNum;
 	}
 	
-	/*		AB = 26*1 + 2 = 28
-			A0 = 26
-			ABCD = (26^3)*1+(26^2)*2+(26^1)*3+(26^0)*4=19010			
-			The following function will convert the input to the string.*/
+	/*	AB = 26*1 + 2 = 28
+	 *	A0 = 26
+	 *	ABCD = (26^3)*1+(26^2)*2+(26^1)*3+(26^0)*4=19010			
+	 *	The following function will convert the input to the string.
+	 */
 	
-	public final void toDecimal(){
+	public final void toDecimal() {
 		double sum =0;
-		char[] array = Znum.toCharArray();
+		char[] array = zNum.toCharArray();
 		double l = (double)array.length;
-		for(int i=0; i<l; i++){
-			double val = (double)(getvalue(array[i]));
+		for(int i=0; i<l; i++) {
+			double val = (double)(getValue(array[i]));
 			sum = sum + (Math.pow(26,(l-i-1)))*(val);
 		}
-		System.out.println("The decimal value of " + Znum + " is " + (int)sum);
+		System.out.println("The decimal value of " + zNum + " is " + (int)sum);
 	}
 	
 	
 	//This function converts the input character to its value and returns it, i.e., A=1, B=2, ... Z=26
-	private int getvalue(char zdigit){
-		if(zdigit == '0')
+	private int getValue(char zDigit) {
+		if(zDigit == '0') {
 			return 0;
-		else{
-			int ascii = (int)zdigit;
+		} else {
+			int ascii = (int)zDigit;
 			return (ascii - 64);
 		}
 	}
